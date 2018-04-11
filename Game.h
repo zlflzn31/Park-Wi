168cfd61e93d343a0084fa4b8813698671a531a1
zlflzn31 <hwi@wustl.edu>
2018-04-10 19:27:51 -0500

    Add files via upload
    game.h

---
+++ Game.h
@@ -0,0 +1,37 @@
+#pragma once
+#ifndef GAME_H_
+#define GAME_H_
+
+#include "Player.h"
+#include "Deck.h"
+#include "Hand.h"
+#include "VerifyCard.h"
+#include <mutex>
+
+using namespace std;
+class Deck;
+struct Player;
+
+class Game {
+
+protected:
+	static shared_ptr<Game> pGame;
+	Deck mainDeck;
+	vector<shared_ptr<Player>> players;
+
+public:
+	static shared_ptr<Game> instance();
+	static void start_game(const string&);
+	static void stop_game();
+
+	ErrorControl add_player(const string&);
+	shared_ptr<Player> find_player(const string&);
+	virtual ~Game(); // maybe = 0;
+	virtual int before_turn(Player&) = 0;
+	virtual int turn(Player&) = 0;
+	virtual int after_turn(Player&) = 0;
+	virtual int before_round() = 0;
+	virtual int round() = 0;
+	virtual int after_round() = 0;
+};
+#endif  //GAME_H_
