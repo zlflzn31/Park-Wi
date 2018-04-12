8a9cc7f61a91d6b8c042b3096e9380dfcd4973ff
zlflzn31 <hwi@wustl.edu>
2018-04-11 21:14:13 -0500

    Add files via upload

---
+++ FiveCardDraw.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "FiveCardDraw.h"
+
+FiveCardDraw::FiveCardDraw()
+{
+	dealer = 0;
+	discardedDeck = Deck();
+
+	deck = Deck();
+	vector<Card> cards = vector<Card>();
+	int suitNum = 4;
+	int rankNum = 13;
+	for (int i = 0; i < suitNum; i++)  
+	{
+		for (int j = 0; j < rankNum; j++) 
+		{
+			Card c;
+			c.s = static_cast<Card::suit>(i); // I used static cast, but I feel that it should be different... 
+			c.r = static_cast<Card::rank>(j); // 
+			deck.add_card(c);
+		}
+	}
+}
+
+int FiveCardDraw::before_round()
+{
+	return 0;
+}
+
+int FiveCardDraw::round()
+{
+	return 0;
+}
+
+int FiveCardDraw::after_round()
+{
+	return 0;
+}
