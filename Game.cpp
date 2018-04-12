bd1ec7630d52e1de3ebe15447c16d0c08d047154
zlflzn31 <hwi@wustl.edu>
2018-04-11 20:43:56 -0500

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -18,17 +18,17 @@ shared_ptr<Game> Game::instance() {
 
 // start_game method
 void Game::start_game(const string& s) {
-	string f = "FiveCardDraw";
-	if (pGame != nullptr) { // check if the static pointer member variable is non-singular
-		throw runtime_error("Error: game_already_started");
-	}
-	else if (s.find(f) == string::npos) { // check if the string contains "FiveCardDraw"
-		throw runtime_error("Error: unknown_game");
-	}
-	else { // dynamically allocate an instance of FiveCardDraw and store its address in pGame
-		shared_ptr<FiveCardDraw> fcd = make_shared<FiveCardDraw>(); 
-		pGame = fcd;
-	}
+//	string f = "FiveCardDraw";
+//	if (pGame != nullptr) { // check if the static pointer member variable is non-singular
+//		throw runtime_error("Error: game_already_started");
+//	}
+//	else if (s.find(f) == string::npos) { // check if the string contains "FiveCardDraw"
+//		throw runtime_error("Error: unknown_game");
+//	}
+//	else { // dynamically allocate an instance of FiveCardDraw and store its address in pGame
+//		shared_ptr<FiveCardDraw> fcd = make_shared<FiveCardDraw>();
+//		pGame = fcd;
+//	}
 }
 
 // stop_game method
@@ -43,16 +43,17 @@ void Game::stop_game() {
 ErrorControl Game::add_player(const string & givenPlayer)
 {
 	string playerName = givenPlayer; // name of "given" player's
-	//this for loop checks if given player's name is same with players' name of the vector. 
-	for (vector<shared_ptr<Player>>::iterator it = playersVec.begin(); it != playersVec.end(); ++it) 
+									 //this for loop checks if given player's name is same with players' name of the vector. 
+	for (vector<shared_ptr<Player>>::iterator it = playersVec.begin(); it != playersVec.end(); ++it)
 	{
 		if ((*it)->playerName == playerName) { // compare playername of each Player of the vector to "given" player's name. 
 			throw runtime_error("Error: already_playing");
 		}
-	} 
+	}
 	//this below logic push backs the given player's name into the vector of the players. 
 	vector<char> playerNameCStyle(playerName.length() + 1);
-	strcpy(&playerNameCStyle[0], playerName.c_str());
+	playerName.c_str();
+	strcpy_s(&playerNameCStyle[0], sizeof(playerNameCStyle[0]), givenPlayer.c_str());
 	char* pnc = &playerNameCStyle[0];
 	Player temp(pnc);
 	playersVec.push_back(make_shared<Player>(temp));
@@ -62,7 +63,7 @@ ErrorControl Game::add_player(const string & givenPlayer)
 shared_ptr<Player> Game::find_player(const string & givenPlayer)
 {
 	//a logic to iterate vector<shared_ptr<Playrer>> in game class. 
-	for (vector<shared_ptr<Player>>::iterator it = playersVec.begin(); it != playersVec.end(); ++it) 
+	for (vector<shared_ptr<Player>>::iterator it = playersVec.begin(); it != playersVec.end(); ++it)
 	{
 		if ((*it)->playerName == givenPlayer) {
 			return *it;
