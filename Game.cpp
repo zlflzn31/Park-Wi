da6b2c80c15cbd8aced9b02379d68a1b7d5d2c37
jongwhanpark <35941051+jongwhanpark@users.noreply.github.com>
2018-04-12 19:18:13 -0500

    Add files via upload

--- Game.cpp
+++ Game.cpp
@@ -49,17 +49,17 @@ int Game::get_num_player() {
 //vector<shared_ptr<Player>> players;
 ErrorControl Game::add_player(const string & givenPlayer)
 {
-	string playerName = givenPlayer; // name of "given" player's
+	string givenName = givenPlayer; // name of "given" player's
 									 //this for loop checks if given player's name is same with players' name of the vector. 
 	for (vector<shared_ptr<Player>>::iterator it = playersVec.begin(); it != playersVec.end(); ++it)
 	{
-		if ((*it)->playerName == playerName) { // compare playername of each Player of the vector to "given" player's name. 
+		if ((*it)->playerName == givenName) { // compare playername of each Player of the vector to "given" player's name. 
 			throw runtime_error("Error: already_playing");
 		}
 	}
 	//this below logic push backs the given player's name into the vector of the players. 
-	vector<char> playerNameCStyle(playerName.length() + 1);
-	playerName.c_str();
+	vector<char> playerNameCStyle(givenName.length() + 1);
+	givenName.c_str();
 	strcpy_s(&playerNameCStyle[0], sizeof(playerNameCStyle[0]), givenPlayer.c_str());
 	char* pnc = &playerNameCStyle[0];
 	Player temp(pnc);
