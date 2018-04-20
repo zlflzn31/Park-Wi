a3c596307de1c09e321cddc63b8bd83ad5d83105
jongwhanpark <35941051+jongwhanpark@users.noreply.github.com>
2018-04-20 17:32:23 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -36,6 +36,7 @@ public:
 	ErrorControl add_player(const string&);
 	shared_ptr<Player> find_player(const string&);
 	int get_num_player();
+	int busted();
 	virtual int before_turn(Player&) = 0;
 	virtual int turn(Player&) = 0;
 	virtual int after_turn(Player&) = 0;
