7fbae200ac5e649777c8a4bf585cca96f83b0265
zlflzn31 <hwi@wustl.edu>
2018-04-25 22:18:44 -0500

    Update PokerGame.h

--- PokerGame.h
+++ PokerGame.h
@@ -13,6 +13,7 @@
 #include <vector> 
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 class PokerGame : public Game
 {
@@ -20,16 +21,17 @@ class PokerGame : public Game
 
 public:
 	PokerGame();
-
+	virtual int before_round() = 0;
+	virtual int round() = 0;
+	virtual int after_round();
 protected:
 	size_t cards_per_hand;
 	size_t dealer;
 	Deck discardedDeck; // discard deck. 
 	Deck mainDeck; // main deck. 
-
-	virtual int before_round() = 0;
-	virtual int round() = 0;
-	virtual int after_round();
+	unsigned int foldCounts;  // numOfFold: number of players who fold
+	void betting();
+	
 };
 
 bool poker_rank(const shared_ptr<Player>&, const shared_ptr<Player>&);
