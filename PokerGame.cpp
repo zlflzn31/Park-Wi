bec358ce0bf1fdb2ec5f536ab699943521de6fa2
zlflzn31 <hwi@wustl.edu>
2018-04-25 22:19:22 -0500

    Update PokerGame.cpp

--- PokerGame.cpp
+++ PokerGame.cpp
@@ -1,20 +1,19 @@
 #include "stdafx.h"
-#include "FiveCardDraw.h"
 #include "PokerGame.h"
-#include <algorithm>
+#include "GameExceptions.h"
 
 PokerGame::PokerGame()
 {
 	dealer = 0;
 	discardedDeck = Deck();
-
+	foldCounts = 0;
 	mainDeck = Deck();
 	vector<Card> cards = vector<Card>();
 	int suitNum = 4;
 	int rankNum = 13;
-	for (int i = 0; i < suitNum; i++)
+	for (int i = 0; i < suitNum; ++i)
 	{
-		for (int j = 0; j < rankNum; j++)
+		for (int j = 0; j < rankNum; ++j)
 		{
 			Card c;
 			c.s = static_cast<Card::suit>(i); // I used static cast, but I feel that it should be different... 
@@ -41,6 +40,372 @@ bool poker_rank(const shared_ptr<Player>& p1, const shared_ptr<Player>& p2)
 	}
 }
 
+void PokerGame::betting()
+{	// isBet: is true if someone bets in a rotation
+	// isRaise: is true if someone raises in a rotation
+	// bet: the highest bet in the phase
+	bool betted = false, raised = false;
+	unsigned int betValue = 0;
+
+	// reset all players' record
+	for (int i = 0; i < playersVec.size(); ++i)
+	{
+		playersVec[i]->betCount = 0;
+	}
+	// I just found that this for loop seems much easier to implement, since I  dont have to write, 
+	// playersVec[i] for every p. 
+	for (auto p : playersVec)
+	{
+		if (!p->isFold)   // if the player is in the round
+		{
+			cout << endl << p->playerName << ": ";
+			if (p->noChip)
+			{
+				cout << "No chips. You have to stay until the game ends." << endl;
+			}
+			else
+			{
+				cout << "Which action will you talk ? Enter : ";
+				if (!betted)  // no one has bet, so the beginning stage.
+				{
+					cout << " bet_one_chip, " << "bet_two_chips, " << ", or check. " << endl;
+					string ans;
+					while (cin >> ans)
+					{
+						if (ans == "bet_one_chip")
+						{
+							betValue = 1;
+							cout << "You have betted: " << betValue << " times. " <<  endl 
+							<< endl;
+							betted = true;
+							p->chip = p->chip - betValue;
+							p->betCount = betValue;
+							break;
+						}
+						else if (ans == "bet_two_chips")
+						{
+							if (p->chip > 1)
+							{
+								betValue = 2;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								betted = true;
+								p->chip = p->chip - betValue;
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is 1. You can either bet one chip or check." << endl;
+							}
+						}
+						else if (ans == "check")
+						{
+							cout << "You have betted: " << betValue << " times. " << endl
+							<< endl;
+							break;
+						}
+						else
+						{
+							cout << "ENTER A VAILD OPTION. " << endl;
+						}
+					}
+				}
+				else // if betted. 
+				{
+					cout << " raise_one_chip, " << "raise_two_chips, " << "call, " << "or fold." << endl;
+					string ans;
+					while (cin >> ans)
+					{
+						if (ans == "raise_one_chip")
+						{
+							if (p->chip > betValue)
+							{
+								betValue = betValue + 1;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								raised = true;
+								p->chip = p->chip - betValue;
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You mat try other action." << endl;
+							}
+						}
+						else if (ans == "raise_two_chips")
+						{
+							if (p->chip > betValue + 1)
+							{
+								betValue = betValue + 2;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								raised = true;
+								p->chip = p->chip - betValue;
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You mat try other action." << endl;
+							}
+						}
+						else if (ans == "call")
+						{
+							if (p->chip >= betValue)
+							{
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								p->chip = p->chip - betValue;
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You will bet all of your chips to the pot." 
+									 << "You will stay until the round is over." << endl;
+								p->chip = 0;
+								p->betCount = betValue;
+								break;
+							}
+						}
+						else if (ans == "fold")
+						{
+							cout << "You have betted: " << betValue << " times. " << endl
+							<< endl;
+							for (int i = 0; i < p->playerHand.size(); ++i)
+							{
+								Card c = p->playerHand[i];
+								p->playerHand.remove_card(i);
+								discardedDeck.add_card(c);
+							}
+							p->isFold = true;
+							++foldCounts;
+							pot = pot + p->betCount;
+							p->betCount = 0;
+							break;
+						}
+						else
+						{
+							cout << "ENTER A VAILD OPTION. " << endl;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (betted)  //if betted previously 
+	{
+		for (auto p : playersVec)
+		{
+			if ((!p->isFold) && (p->betCount < betValue)) 
+			{
+				cout << endl << p->playerName;
+				if (p->noChip())
+				{
+					cout << ": No chips. You have to stay until the game ends." << endl;
+				}
+				else
+				{
+					cout << ": raise_one_chip, " << "raise_two_chips, " << "call, " << "or fold." << endl;
+					string ans;
+					while (cin >> ans)
+					{
+						if (ans == "raise_one_chip")
+						{
+							if (p->chip > betValue)
+							{
+								betValue = betValue + 1;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								raised = true;
+								p->chip = p->chip - (betValue - p->betCount);
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You mat try other action." << endl;
+							}
+						}
+						else if (ans == "raise_2_chips")
+						{
+							if (p->chip > betValue + 1)
+							{
+								betValue = betValue + 2;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								raised = true;
+								p->chip = p->chip - (betValue - p->betCount);
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You mat try other action." << endl;
+							}
+						}
+						else if (ans == "call")
+						{
+							if (p->chip >= betValue)
+							{
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								p->chip = p->chip - (betValue - p->betCount);
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You will bet all of your chips to the pot."
+									<< "You will stay until the round is over." << endl;
+								p->chip = 0;
+								p->betCount = betValue;
+								break;
+							}
+						}
+						else if (ans == "fold")
+						{
+							cout << "You have betted: " << betValue << " times. " << endl
+							<<endl;
+							for (int i = 0; i < p->playerHand.size(); ++i)
+							{
+
+								Card c = p->playerHand[i];
+								p->playerHand.remove_card(i);
+								discardedDeck.add_card(c);
+							}
+							p->isFold = true;
+							++foldCounts;
+							pot = pot + p->betCount;
+							p->betCount = 0;
+							break;
+						}
+						else
+						{
+							cout << "PLEASE ENTER A VALID OPTION" << endl;
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+
+	
+
+
+	while (raised)  // if raised previously 
+	{
+		raised = false;
+		for (auto p : playersVec)
+		{
+			if ((!p->isFold) && (p->betCount < betValue)) 
+			{
+				cout << p->playerName << endl;
+				if (p->noChip())
+				{
+					cout << ": No chips. You have to stay until the game ends." << endl;
+				}
+				else
+				{
+					cout << ": raise_one_chip, " << "raise_two_chips, " << "call, " << "or fold." << endl;
+					string ans;
+					while (cin >> ans)
+					{
+						if (ans == "raise_1_chip")
+						{
+							if (p->chip > betValue)
+							{
+								betValue = betValue + 1;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								raised = true;
+								p->chip = p->chip - (betValue - p->betCount);
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You mat try other action." << endl;
+							}
+						}
+						else if (ans == "raise_2_chips")
+						{
+							if (p->chip > betValue + 1)
+							{
+								betValue = betValue + 2;
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								raised = true;
+								p->chip = p->chip - (betValue - p->betCount);
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You mat try other action." << endl;
+							}
+						}
+						else if (ans == "call")
+						{
+							if (p->chip >= betValue)
+							{
+								cout << "You have betted: " << betValue << " times. " << endl
+								<< endl;
+								p->chip = p->chip - (betValue - p->betCount);
+								p->betCount = betValue;
+								break;
+							}
+							else
+							{
+								cout << "Your current chip value is : " << p->chip << ". You will bet all of your chips to the pot."
+									<< "You will stay until the round is over." << endl;
+								p->chip = 0;
+								p->betCount = betValue;
+								break;
+							}
+						}
+						else if (ans == "fold")
+						{
+							cout << "Cumulative bet is now " << betValue << endl;
+							for (int i = 0; i < p->playerHand.size(); ++i)
+							{
+								Card c = p->playerHand[i];
+								p->playerHand.remove_card(i);
+								discardedDeck.add_card(c);
+							}
+							p->isFold = true;
+							++foldCounts;
+							pot = pot + p->betCount;
+							p->betCount = 0;
+							break;
+						}
+						else
+						{
+							cout << "PLEASE ENTER A VALID OPTION." << endl;
+						}
+					}
+				}
+			}
+		}
+	}
+	// after the betting phase, push all the remaining players' bets into the pot
+	for (auto p : playersVec)
+	{
+		if (!p->isFold)
+		{
+			pot = pot + p->betCount;
+		}
+	}
+}
+
+
+
+
 //after_round method
 int PokerGame::after_round()
 {
@@ -48,7 +413,10 @@ int PokerGame::after_round()
 	vector<shared_ptr<Player>> temp;
 	for (size_t i = 0; i < playersVec.size(); ++i)
 	{
-		temp.push_back(playersVec[i]);
+		if (!temp[i]->isFold)
+		{
+			temp.push_back(playersVec[i]);
+		}
 	}
 	sort(temp.begin(), temp.end(), [&](shared_ptr<Player>& p1, shared_ptr<Player>& p2) { return poker_rank(p1, p2); });
 
@@ -65,7 +433,7 @@ int PokerGame::after_round()
 	cout << endl;
 	for (size_t i = 0; i < temp.size(); ++i)
 	{
-		cout << "player name: " << temp[i]->playerName << "\nnumber of wins: " << temp[i]->winCounts << "\nnumber of losses: " << temp[i]->lossCounts << "\nplayer's hand: " << temp[i]->playerHand << endl;
+		cout << "player name: " << temp[i]->playerName << "\nnumber of wins: " << temp[i]->winCounts << "\nnumber of losses: " << temp[i]->lossCounts << "\nnumber of chips: " << temp[i]->chip << "\nplayer's hand: " << temp[i]->playerHand << endl;
 
 	}
 	mainDeck.deck.clear();
@@ -77,9 +445,9 @@ int PokerGame::after_round()
 	vector<Card> cardsToMain = vector<Card>();
 	int suitNum = 4;
 	int rankNum = 13;
-	for (int i = 0; i < suitNum; i++)
+	for (int i = 0; i < suitNum; ++i)
 	{
-		for (int j = 0; j < rankNum; j++)
+		for (int j = 0; j < rankNum; ++j)
 		{
 			Card c;
 			c.s = static_cast<Card::suit>(i);
@@ -94,53 +462,8 @@ int PokerGame::after_round()
 	// lab4. for players that lost all their chips, make them decide between resetting their chip count and quitting the game
 	busted();
 
-	//ask the rest of the players whether to leave the game
-	string leftPlayerName;
-	int indexOfLeftPlayer = -1;
-	bool leave;
-	do {
-		leave = false;
-		cout << endl;
-		cout << "Is there any player whom you want to leave? If there is not, enter 'no'. 'No', 'NO', 'nO' won't be accepted !  " << endl;
 
-		cout << "Player's name: " << endl;
-		cin >> leftPlayerName;
-		if (leftPlayerName == "no")
-		{
-			leave = false;
-		}
-		else {
-			cout << "leave the person!!! " << endl;
-			leave = true; // we want someone to leave the game. 
-		}
-		bool validPlayer = false;
-		for (size_t i = 0; i < playersVec.size(); ++i)
-		{
-			if (leftPlayerName == playersVec[i]->playerName)
-			{
-				validPlayer = true;
-				break;
-			}
-		}
-		if (leave && validPlayer)  // if validPlayer is not true ( meaning that we have an invalid player, so we should skip this part)
-		{
-			ofstream ofs(leftPlayerName + ".txt");
-			ofs << leftPlayerName;
-			ofs.close();
-
-			for (size_t i = 0; i < playersVec.size(); ++i)
-			{
-				if (leftPlayerName == (*playersVec[i]).playerName) {
-					indexOfLeftPlayer = i;
-				}
-			}
-			if (indexOfLeftPlayer != -1) {
-				playersVec.erase(playersVec.begin() + indexOfLeftPlayer);
-			}
-		}
-		indexOfLeftPlayer = -1;
-	} while (leave);
-
+	// --------------- THIS BELOW PART SHOULD DEAL WITH THAT 
 	//ask whether to join the game
 	string joiningPlayer;
 	bool join = false;
@@ -159,11 +482,18 @@ int PokerGame::after_round()
 
 		if (join)
 		{
-			add_player(joiningPlayer); //add_player has a logic to check the duplicates. 
+			try
+			{
+				add_player(joiningPlayer); //add_player has a logic to check the duplicates. 
+			}
+			catch (already_playing)
+			{
+				cout << "There is a player who is already in the game." << endl;
+			}
 			busted(); //lab4: if a player with 0 chips tries to join the game, make him reset or not join
 		}
 	} while (join);
-	cout << endl; 
+	cout << endl;
 
 	//next dealer ! 
 	if (dealer >= playersVec.size() - 1)
@@ -173,6 +503,12 @@ int PokerGame::after_round()
 	else {
 		++dealer;
 	}
+	// if one player exists in the game, then end the game. 
+	if (playersVec.size() == 1)
+	{
+		Game::stop_game();
+	}
 
 	return 0;
 }
+
